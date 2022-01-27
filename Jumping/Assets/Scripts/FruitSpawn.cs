d4fd746107f91e9b1aa2ccc503bf2fdc9d5a6b44
Don <cd155@163.com>
2022-01-27 13:26:46 -0500

    add moving gameobject from right to left

--- Jumping/Assets/Scripts/FruitSpawn.cs
+++ Jumping/Assets/Scripts/FruitSpawn.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitS : MonoBehaviour
+public class FruitSpawn: MonoBehaviour
 {
-    //[SerializeField] GameObject[] fruitPrefab
+    [SerializeField] GameObject[] fruitPrefab;
+    [SerializeField] float secondSpawn = 0.5f;
+    [SerializeField] float minTras;
+    [SerializeField] float maxTras;
+    private float minThrust = 70.0f;
+    private float maxThrust = 120.0f;
+    private float time = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // state Fruitspawn frame by frame
+        StartCoroutine(StartFruitSpawn());
+    }
+
+    IEnumerator StartFruitSpawn()
+    {
+        while(true)
+        {
+            var wanted = Random.Range(minTras, maxTras);
+            var position = new Vector3(transform.position.x, wanted);
+            GameObject gameObject = Instantiate(
+                fruitPrefab[Random.Range(0, fruitPrefab.Length)], position, Quaternion.identity);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left * Random.Range(minThrust, maxThrust));
+            yield return new WaitForSeconds(secondSpawn);
+            Destroy(gameObject, time);
+        }
     }
 
     // Update is called once per frame
