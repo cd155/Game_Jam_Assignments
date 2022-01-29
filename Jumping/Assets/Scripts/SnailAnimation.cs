e6344244a34098c2a466bb4f7767fd6a0be32e85
Don <cd155@163.com>
2022-01-29 11:23:53 -0500

    add snail animation class

---
+++ Jumping/Assets/Scripts/SnailAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailAnimation: MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    bool leftInput;
+    bool rightInput;
+    bool zInput;
+    bool xInput;
+    bool cInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetBool("isJump", rigidbody2D.velocity.y != 0);
+
+        if (leftInput) spriteRenderer.flipX = true;
+        else if (rightInput) spriteRenderer.flipX = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftInput = Input.GetKey(KeyCode.LeftArrow);
+        rightInput = Input.GetKey(KeyCode.RightArrow);
+        zInput = Input.GetKey(KeyCode.Z);
+        xInput = Input.GetKey(KeyCode.X);
+        cInput = Input.GetKey(KeyCode.C);
+    }
+}
