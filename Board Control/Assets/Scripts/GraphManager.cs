bf82929fc9065da37788e736a0caf1b3476bd303
cd155 <github@donchen.page>
2022-02-13 12:56:08 -0500

    make graph invisible at start

--- Board Control/Assets/Scripts/GraphManager.cs
+++ Board Control/Assets/Scripts/GraphManager.cs
@@ -7,7 +7,9 @@ public class GraphManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject graph = GameObject.Find("Graph");
+        graph.SetActive(false);
+        Debug.Log("here");
     }
 
     // Update is called once per frame
