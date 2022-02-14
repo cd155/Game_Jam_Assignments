6b077b95591d01e573e950c34416ffb634f58b63
cd155 <github@donchen.page>
2022-02-13 22:12:19 -0500

    add node class

---
+++ Board Control/Assets/Scripts/Node.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NodeLibrary
+{
+    public class Node
+    {
+        private string name;
+
+        private List <Node> nextList = new List<Node>();
+
+        private List <(Node, string, bool)> previousList = new List<(Node, string, bool)>();
+    
+        public Node(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddNextList(Node node)
+        {
+            nextList.Add(node);
+        }
+
+        public void AddPreviousList(Node node, string name, bool active)
+        {
+            previousList.Add((node, name, active));
+        }
+    }
+}
