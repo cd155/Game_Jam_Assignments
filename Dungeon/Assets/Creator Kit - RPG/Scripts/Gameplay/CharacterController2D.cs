a98f7d9b58494c145ef21e4a38821802e5f5b478
cd155 <github@donchen.page>
2022-04-08 12:10:45 -0400

    add dungeon game

---
+++ Dungeon/Assets/Creator Kit - RPG/Scripts/Gameplay/CharacterController2D.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPGM.Gameplay;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace RPGM.Gameplay
+{
+    /// <summary>
+    /// A simple controller for animating a 4 directional sprite using Physics.
+    /// </summary>
+    public class CharacterController2D : MonoBehaviour
+    {
+        public float speed = 1;
+        public float acceleration = 2;
+        public Vector3 nextMoveCommand;
+        public Animator animator;
+        public bool flipX = false;
+
+        new Rigidbody2D rigidbody2D;
+        SpriteRenderer spriteRenderer;
+        PixelPerfectCamera pixelPerfectCamera;
+
+        enum State
+        {
+            Idle, Moving
+        }
+
+        State state = State.Idle;
+        Vector3 start, end;
+        Vector2 currentVelocity;
+        float startTime;
+        float distance;
+        float velocity;
+
+        void IdleState()
+        {
+            if (nextMoveCommand != Vector3.zero)
+            {
+                start = transform.position;
+                end = start + nextMoveCommand;
+                distance = (end - start).magnitude;
+                velocity = 0;
+                UpdateAnimator(nextMoveCommand);
+                nextMoveCommand = Vector3.zero;
+                state = State.Moving;
+            }
+        }
+
+        void MoveState()
+        {
+            velocity = Mathf.Clamp01(velocity + Time.deltaTime * acceleration);
+            UpdateAnimator(nextMoveCommand);
+            rigidbody2D.velocity = Vector2.SmoothDamp(rigidbody2D.velocity, nextMoveCommand * speed, ref currentVelocity, acceleration, speed);
+            spriteRenderer.flipX = rigidbody2D.velocity.x >= 0 ? true : false;
+        }
+
+        void UpdateAnimator(Vector3 direction)
+        {
+            if (animator)
+            {
+                animator.SetInteger("WalkX", direction.x < 0 ? -1 : direction.x > 0 ? 1 : 0);
+                animator.SetInteger("WalkY", direction.y < 0 ? 1 : direction.y > 0 ? -1 : 0);
+            }
+        }
+
+        void Update()
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    IdleState();
+                    break;
+                case State.Moving:
+                    MoveState();
+                    break;
+            }
+        }
+
+        void LateUpdate()
+        {
+            if (pixelPerfectCamera != null)
+            {
+                transform.position = pixelPerfectCamera.RoundToPixel(transform.position);
+            }
+        }
+
+        void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            pixelPerfectCamera = GameObject.FindObjectOfType<PixelPerfectCamera>();
+        }
+
+        // Customized Script
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            // condition to move object
+            switch (collision.gameObject.name)
+            {
+                case "Chicken1":
+                    MoveChicken (collision);
+                    break;
+                case "Chicken2":
+                    MoveChicken (collision);
+                    break;
+                case "Chicken3":
+                    MoveChicken (collision);
+                    break;
+                case "Chicken4":
+                    MoveChicken (collision);
+                    break;                                            
+                default:
+                    break;
+            }
+        }
+
+        private void MoveChicken(Collision2D collision)
+        {
+            collision.transform.position = new Vector3(
+                collision.transform.position.x + nextMoveCommand.x, 
+                collision.transform.position.y + nextMoveCommand.y,
+                0);
+        }
+    }
+}
